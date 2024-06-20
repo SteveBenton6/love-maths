0b46c52579f2403144fb01c8214c9995cd6e0e1d
Steven Benton <steve@bentonemail.uk>
2024-06-20 13:23:22 +0000

    Doc: Add button listeners in script.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-console.log("Hello World!");
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+});
 
 function runGame() {
 
@@ -34,7 +50,7 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivideQuestion() {
-    
+
 }
 
 
