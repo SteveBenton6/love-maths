fc63b527552d179130597c15c104427cc943e7e9
Steven Benton <steve@bentonemail.uk>
2024-06-20 12:11:42 +0000

    Doc Add JS Function Structure.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
 console.log("Hello World!");
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function IncrementScore() {
+
+
+}
+
+function IncrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+    
+}
+
+
